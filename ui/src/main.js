7e7193827e5ee71d26a6f633a55f26d8c556e673
vanderandrade <vander_c_andrade@hotmail.com>
2021-03-06 19:33:26 -0300

    Removed unused modules

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Toast from 'vue-easy-toast'
-import Autocomplete from 'v-autocomplete'
-import 'v-autocomplete/dist/v-autocomplete.css'
 
 
 Vue.config.productionTip = false
