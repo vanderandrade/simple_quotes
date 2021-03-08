6d92dec53e5723627579052a105a45ade4143628
vanderandrade <vander_c_andrade@hotmail.com>
2021-03-08 19:09:41 -0300

    Added test db config and test db variable

--- api/config.py
+++ api/config.py
@@ -9,3 +9,13 @@ class DeployConfig(object):
     pgdb = os.environ.get('PGDATABASE')
     SQLALCHEMY_DATABASE_URI = f'postgres://{pguser}:{pgpass}@{pghost}:{pgport}/{pgdb}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestConfig(object):
+    DEBUG = True
+    pguser = os.environ.get('PGUSER')
+    pgpass = os.environ.get('PGPASSWORD')
+    pghost = os.environ.get('PGHOST')
+    pgport = os.environ.get('PGPORT')
+    pgdb = os.environ.get('PGDATABASETEST')
+    SQLALCHEMY_DATABASE_URI = f'postgres://{pguser}:{pgpass}@{pghost}:{pgport}/{pgdb}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
